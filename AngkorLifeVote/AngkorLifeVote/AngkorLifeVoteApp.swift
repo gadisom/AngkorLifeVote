97817443624ef725aa91f603d044d8e2d0aec256
gadisom <jeongwon.kim@gmail.com>
2025-01-23 20:08:27 +0900

    Add: XcodeProject

---
+++ AngkorLifeVote/AngkorLifeVote/AngkorLifeVoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngkorLifeVoteApp.swift
+//  AngkorLifeVote
+//
+//  Created by 김정원 on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct AngkorLifeVoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
