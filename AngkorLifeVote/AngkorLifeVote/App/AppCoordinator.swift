9faeead14c5f5840ddee5894c664c7b7081af09a
gadisom <jeongwon.kim@gmail.com>
2025-01-23 20:50:08 +0900

    feat: AppCoordinator 패턴 기본 함수 추가

--- AngkorLifeVote/AngkorLifeVote/App/AppCoordinator.swift
+++ AngkorLifeVote/AngkorLifeVote/App/AppCoordinator.swift
@@ -5,4 +5,32 @@
 //  Created by 김정원 on 1/23/25.
 //
 
-import Foundation
+import SwiftUI
+
+final class AppCoordinator: ObservableObject {
+    @Published var currentView: AnyView = .init(EmptyView())
+    
+//    let userSession = UserSession()
+//    let networkService = NetworkService()
+    init() {
+        start()
+    }
+    
+    func start() {
+        
+    }
+    
+    func showLoginFlow() {
+        
+        
+    }
+    
+    func showMainFlow() {
+        
+    }
+    
+    func showProfileFlow() {
+        
+    }
+}
+
