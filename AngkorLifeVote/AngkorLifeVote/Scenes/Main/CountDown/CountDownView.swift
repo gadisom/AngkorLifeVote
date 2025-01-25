edb2589e1c274f51a0076ea6bf2113c9db3da2f9
gadisom <jeongwon.kim@gmail.com>
2025-01-25 14:47:53 +0900

    Feat: CountDownView 생성

---
+++ AngkorLifeVote/AngkorLifeVote/Scenes/Main/CountDown/CountDownView.swift
@@ -0,0 +1,18 @@
+//
+//  CountDownView.swift
+//  AngkorLifeVote
+//
+//  Created by 김정원 on 1/25/25.
+//
+
+import SwiftUI
+
+struct CountDownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountDownView()
+}
